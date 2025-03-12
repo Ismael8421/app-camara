dbe1faac18719db312dbb4311930c72333e6855f
ismaelparapi40 <ismaelparapi40@gmail.com>
2025-03-12 20:33:03 +0000

    Revert "commit app-camara4"
    This reverts commit 5c33f42e8118de1224128e2cbb01e1528eff07cb.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ismael8421.app',
   appName: 'app-camara',
-  webDir: 'dist/app-camara/browser'
+  webDir: 'dist'
 };
 
 export default config;
